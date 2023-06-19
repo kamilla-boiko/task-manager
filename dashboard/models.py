f1000e9328d35c0e8c5bd9e294115cccb5c53f38
kamilla-boiko <kamilla.boyko@gmail.com>
2023-06-19 14:32:58 +0300

    BACK: MODELS: Add models Task, TaskType, Position, Worker to models.py
    Create models according to DB-structure scheme

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,40 @@
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class TaskType(models.Model):
+    name = models.CharField(max_length=63)
+
+
+class Position(models.Model):
+    name = models.CharField(max_length=63)
+
+
+class Worker(AbstractUser):
+    position = models.ForeignKey(
+        Position,
+        on_delete=models.CASCADE,
+        related_name="workers"
+    )
+
+
+class Task(models.Model):
+    PRIORITY_CHOICES = (
+        ("Urgent", "Urgent"),
+        ("High", "High"),
+        ("Medium", "Medium"),
+        ("Low", "Low"),
+    )
+
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    deadline = models.DateField()
+    is_completed = models.BooleanField(default=False)
+    priority = models.CharField(max_length=10, choices=PRIORITY_CHOICES)
+    task_type = models.ForeignKey(
+        TaskType,
+        on_delete=models.CASCADE,
+        related_name="tasks"
+    )
+    assignees = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="tasks")
