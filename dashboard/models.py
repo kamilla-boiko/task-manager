c3ba3d5c9f11a4f9260d7ee9e5d9c1c492a0cde8
kamilla-boiko <kamilla.boyko@gmail.com>
2023-06-20 14:44:22 +0300

    BACK: MODEL: Set option null to field position in Worker
    Setting the null=True option on the position field of the Worker model allow
    the position field to be optional to fix the integrity error with creating a new user.

--- dashboard/models.py
+++ dashboard/models.py
@@ -15,7 +15,8 @@ class Worker(AbstractUser):
     position = models.ForeignKey(
         Position,
         on_delete=models.CASCADE,
-        related_name="workers"
+        related_name="workers",
+        null=True
     )
 
 
