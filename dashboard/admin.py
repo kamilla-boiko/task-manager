b992eaddc99f0dbaeb835f6eccb1c63173018699
kamilla-boiko <kamilla.boyko@gmail.com>
2023-06-20 14:50:04 +0300

    BACK: ADMIN: Create admin.py with models registrations
    To ease of interaction customize some fields in TaskAdmin and WorkerAdmin
    in the admin interface. The TaskType and Position models are registered as well.

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from dashboard.models import Task, TaskType, Position, Worker
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("name", "deadline", "is_completed", "priority", "task_type")
+    list_filter = ("is_completed", "priority", "task_type")
+    search_fields = ("name",)
+
+
+@admin.register(Worker)
+class WorkerAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("position",)
+    fieldsets = UserAdmin.fieldsets + (
+        ("Additional info", {"fields": ("position",)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("Additional info", {"fields": ("first_name", "last_name", "position")}),
+    )
+
+
+admin.site.register(TaskType)
+admin.site.register(Position)
