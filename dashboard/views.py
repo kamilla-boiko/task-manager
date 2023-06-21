64c1b82e69ac54f1ce1f610c68a2384a439997b0
kamilla-boiko <kamilla.boyko@gmail.com>
2023-06-21 14:17:30 +0300

    BACK: VIEWS: Add new view for home page endpoint

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+from dashboard.models import Task, Worker
+
+
+def index(request):
+    num_tasks = Task.objects.count()
+    tasks_completed = Task.objects.filter(is_completed=True).count()
+    tasks_uncompleted = num_tasks - tasks_completed
+
+    context = {
+        "num_tasks": num_tasks,
+        "tasks_completed": tasks_completed,
+        "tasks_uncompleted": tasks_uncompleted
+    }
+
+    return render(request, "dashboard/index.html", context=context)
