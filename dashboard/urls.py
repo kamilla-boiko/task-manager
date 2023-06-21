4846bf7841a42a0117951be75c9365020ec52786
kamilla-boiko <kamilla.boyko@gmail.com>
2023-06-21 20:11:23 +0300

    BACK: URLS: Add endpoint for task type list

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 
-from dashboard.views import index
+from dashboard.views import index, TaskTypeListView
 
 urlpatterns = [
-    path("", index, name="index")
+    path("", index, name="index"),
+    path(
+        "task-types/",
+        TaskTypeListView.as_view(),
+        name="task-type-list"
+    )
 ]
 
 app_name = "dashboard"
