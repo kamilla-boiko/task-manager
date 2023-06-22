f96bd656e4fba6474a9057e196ebfc5da6c30a00
kamilla-boiko <kamilla.boyko@gmail.com>
2023-06-22 16:11:26 +0300

    BACK: VIEWS: Add view for position update form

--- dashboard/views.py
+++ dashboard/views.py
@@ -56,6 +56,13 @@ class PositionCreateView(generic.CreateView):
     template_name = "dashboard/position_form.html"
 
 
+class PositionUpdateView(generic.UpdateView):
+    model = Position
+    fields = "__all__"
+    success_url = reverse_lazy("dashboard:position-list")
+    template_name = "dashboard/position_form.html"
+
+
 class TaskListView(generic.ListView):
     model = Task
     paginate_by = 5
