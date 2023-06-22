bde86f53dadc582d7284635f19410f5f98bf6156
kamilla-boiko <kamilla.boyko@gmail.com>
2023-06-22 19:16:49 +0300

    BACK: FORMS: Add Worker creation form
    It is important to secure and hash password

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -2,9 +2,18 @@ from datetime import date
 
 from django import forms
 from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
 from django.core.exceptions import ValidationError
 
-from dashboard.models import Task
+from dashboard.models import Task, Worker
+
+
+class WorkerCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = Worker
+        fields = UserCreationForm.Meta.fields + (
+            "first_name", "last_name", "email", "position"
+        )
 
 
 class TaskForm(forms.ModelForm):
