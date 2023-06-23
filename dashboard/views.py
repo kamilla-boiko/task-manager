27d00fbcf9af1dd6bbc2f30721aa0239db60148e
kamilla-boiko <kamilla.boyko@gmail.com>
2023-06-23 12:25:51 +0300

    BACK: VIEWS: Add search field by name to Task list

--- dashboard/views.py
+++ dashboard/views.py
@@ -2,7 +2,15 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
 
-from dashboard.forms import TaskForm, WorkerCreationForm, WorkerPositionUpdateForm, WorkerSearchForm, WorkerFilterForm
+from dashboard.forms import (
+    TaskForm,
+    TaskSearchForm,
+    WorkerCreationForm,
+    WorkerPositionUpdateForm,
+    WorkerSearchForm,
+    WorkerFilterForm,
+)
+
 from dashboard.models import Task, TaskType, Position, Worker
 
 
@@ -73,7 +81,28 @@ class PositionDeleteView(generic.DeleteView):
 class TaskListView(generic.ListView):
     model = Task
     paginate_by = 5
-    queryset = Task.objects.select_related("task_type")
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(TaskListView, self).get_context_data(**kwargs)
+
+        name = self.request.GET.get("name", "")
+
+        context["search_form"] = TaskSearchForm(
+            initial={"name": name}
+        )
+
+        return context
+
+    def get_queryset(self):
+        queryset = Task.objects.select_related("task_type")
+        form = TaskSearchForm(self.request.GET)
+
+        if form.is_valid():
+            queryset = queryset.filter(
+                name__icontains=form.cleaned_data["name"]
+            )
+
+        return queryset
 
 
 class TaskDetailView(generic.DetailView):
