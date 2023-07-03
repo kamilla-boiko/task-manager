a146432aeedcdb14de49faca7a85a4b1a32fa4a3
kamilla-boiko <kamilla.boyko@gmail.com>
2023-07-03 16:34:39 +0300

    Set up DEBUG with .env

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
+DEBUG = os.environ.get("DEBUG", default="False") == "True"
 
 ALLOWED_HOSTS = ["127.0.0.1"]
 
